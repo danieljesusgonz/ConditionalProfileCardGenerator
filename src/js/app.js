0ea6b4091c1ec3fefa97ff23e16457a7589c553a
danieljesusgonz <danieljesusgonz@gmail.com>
2022-09-18 21:17:03 +0000

    social media

--- src/js/app.js
+++ src/js/app.js
@@ -40,12 +40,21 @@ function render(variables = {}) {
           <h3> ${variables.city == null ? "City," : variables.city} ${
     variables.country == null ? "Country." : variables.country
   } </h3>
-          <ul class=${variables.socialMediaPosition}
-            <li><a href="variable.twitter"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="variable.github"><i class="fab fa-github"></i></a></li>
-            <li><a href="variable.linkedin"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="variable.instagram"><i class="fab fa-instagram"></i></a></li>
-          </ul>
+  
+  <ul class="${variables.socialMediaPosition}">
+  <li><a href="https://twitter.com/${
+    variables.twitter
+  }"><i class="fab fa-twitter"></i></a></li>
+  <li><a href="https://github.com/${
+    variables.github
+  }"><i class="fab fa-github"></i></a></li>
+  <li><a href="https://linkedin.com/${
+    variables.linkedin
+  }"><i class="fab fa-linkedin"></i></a></li>
+  <li><a href="https://instagram.com/${
+    variables.instagram
+  }"><i class="fab fa-instagram"></i></a></li>
+</ul>
         </div>
     `;
 }
